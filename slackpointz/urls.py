3bedabe64d76f570dfb75fbe15b8bef8ed64fa78
Hugo Delahousse <hugo.delahousse@gmail.com>
2018-09-11 22:42:20 -0400

    Use pointz as an app in urls

--- slackpointz/urls.py
+++ slackpointz/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import pointz.urls
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pointz/', pointz.urls)
+    path('pointz/', include('pointz.urls')),
 ]
