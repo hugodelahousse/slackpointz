af9efb1ffd80c384539e986b4f700dec73f39a00
Hugo Delahousse <hugo.delahousse@gmail.com>
2018-09-12 23:47:45 -0400

    Make it impossible to give yourself points
    Closes #1

--- pointz/views.py
+++ pointz/views.py
@@ -8,7 +8,7 @@ from .models import SlackUser
 logger = logging.getLogger('django')
 NO_TEXT = ['Gimme some info dawg !']
 
-slash_pattern = re.compile(r'(?P<user><@(?P<user_id>U[A-Z0-9]+)\|(?P<username>\S+)>)( (?P<score_delta>[+-]?\d+))?')
+slash_pattern = re.compile(r'(?P<user><@(?P<user_id>U[A-Z0-9]+)(\|(?P<username>\S+))?>)( (?P<score_delta>[+-]?\d+))?')
 
 
 def slack_response(text, response_type="in_channel"):
@@ -35,13 +35,15 @@ def slash_pointz(request):
     username = result.group('username')
     score_delta = result.group('score_delta')
 
-    if not user_id or not username:
+    if not user_id:
         return HttpResponse(status=400)
 
     user, created = SlackUser.objects.get_or_create(user_id=user_id)
 
     if score_delta:
+        if user.user_id == request.POST.get('user_id'):
+            return HttpResponse('You can\'t give yourself points, silly !')
         user.increase_score(score_delta)
-        return slack_response(f'@{username}\'s score is now: {user.score}')
+        return slack_response(f'@{username or "this user"}\'s score is now: {user.score}')
     elif result.group('user_id'):
         return slack_response(f'@{username} has {user.score} points !')
